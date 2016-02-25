6647324589eeccf772a919b7007d36bfd325caf9
Antonis Flangofas <a.flangofas@qobo.biz>
2016-02-25 15:55:11 +0200

    Forced views to use admin layout (task #1297)

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -7,4 +7,16 @@ use App\Controller\AppController as BaseController;
 class AppController extends BaseController
 {
 
+    /**
+     * Initialization hook method.
+     *
+     * Use this method to add common initialization code like loading components.
+     *
+     * @return void
+     */
+    public function initialize()
+    {
+        parent::initialize();
+        $this->viewBuilder()->layout('QoboAdminPanel.basic');
+    }
 }
