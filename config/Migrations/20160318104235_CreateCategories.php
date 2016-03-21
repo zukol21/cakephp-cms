1f3223cc951c2775afd9762830351930c47088b9
Antonis Flangofas <a.flangofas@qobo.biz>
2016-03-21 15:35:51 +0200

    Created migrations (task #1477)

---
+++ config/Migrations/20160318104235_CreateCategories.php
@@ -0,0 +1,52 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateCategories extends AbstractMigration
+{
+
+    public $autoId = false;
+
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('categories');
+        $table->addColumn('id', 'uuid', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('slug', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addIndex([
+            'slug',
+        ], [
+            'name' => 'UNIQUE_SLUG',
+            'unique' => true,
+        ]);
+        $table->addPrimaryKey([
+            'id',
+        ]);
+        $table->create();
+    }
+}
