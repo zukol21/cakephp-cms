687756483d811f46d81d37f929c2d927a2684dba
Antonis Flangofas <a.flangofas@qobo.biz>
2016-03-29 15:01:55 +0300

    Removed comment (task #1516)

--- config/routes.php
+++ config/routes.php
@@ -4,7 +4,6 @@ use Cake\Routing\Router;
 Router::plugin(
     'Cms',
     function ($routes) {
-        // Frontend requests only
         $routes->extensions(['json']);
         $routes->fallbacks('DashedRoute');
     }
