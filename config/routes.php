6fa69d7a7aba67e4d40d005fa72f0cc686f08e4e
Antonis Flangofas <a.flangofas@qobo.biz>
2016-03-29 14:59:49 +0300

    Removed additional routes (task #1516)

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,6 @@ Router::plugin(
     'Cms',
     function ($routes) {
         // Frontend requests only
-        $routes->connect('/categories/*', ['plugin' => 'Cms', 'controller' => 'Categories', 'action' => 'display']);
         $routes->extensions(['json']);
         $routes->fallbacks('DashedRoute');
     }
