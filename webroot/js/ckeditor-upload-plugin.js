c711b54a9e7e75f6230645b1762d21059b063453
Antonis Flangofas <a.flangofas@qobo.biz>
2016-04-06 10:02:46 +0300

    Removed unnecessary styles (task #1677)

--- webroot/js/ckeditor-upload-plugin.js
+++ webroot/js/ckeditor-upload-plugin.js
@@ -3,8 +3,6 @@
 // @link: http://ckeditor.com/addon/imagepaste
 var uploadOptions = {
     extraPlugins: 'uploadimage,image2',
-    // Load the default contents.css file plus customizations for this sample.
-    contentsCss: [ CKEDITOR.basePath + 'contents.css', 'http://sdk.ckeditor.com/samples/assets/css/widgetstyles.css' ],
     uploadUrl: '/cms/articles/upload-from-editor.json',
     // Configure the Enhanced Image plugin to use classes instead of styles and to disable the
     // resizer (because image size is controlled by widget styles or the image takes maximum
