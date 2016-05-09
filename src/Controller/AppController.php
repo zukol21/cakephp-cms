e99ca53c89c0f9343a786edc930cf913c77815b4
Antonis Flangofas <flangofas@users.noreply.github.com>
2016-05-09 15:58:29 +0300

    Removed forced layout (task #1957)

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -19,27 +19,5 @@ class AppController extends BaseController
     {
         parent::initialize();
         $this->loadComponent('RequestHandler');
-        if (!$this->_isFrontEndAction()) {
-            //Set the backend layout
-            $this->viewBuilder()->layout('QoboAdminPanel.basic');
-        }
     }
-
-    /**
-     * This funtion defines the frontend actions of the plugin which are
-     * less than the backend actions.
-     *
-     * @param  string  $action given action|fallback to Request's action
-     * @return boolean         Boolean flag
-     */
-    protected function _isFrontEndAction($action = null)
-    {
-        $frontendActions = ['display'];
-        if (is_null($action)) {
-            $action = $this->request->params['action'];
-        }
-
-        return in_array($action, $frontendActions);
-    }
-
 }
