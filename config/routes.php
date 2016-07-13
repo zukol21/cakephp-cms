f77e0e7af91d430024ba063a8f225123df01fe2e
Antonis Flangofas <a.flangofas@qobo.biz>
2016-07-13 19:08:29 +0300

    Routes improvement (task #2272)

--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,12 @@ use Cake\Routing\Router;
 Router::plugin(
     'Cms',
     function ($routes) {
+        $routes->connect('/articles/display/*', ['Plugin' => 'Cms', 'controller' => 'Articles', 'action' => 'display']);
+        $routes->connect('/categories/display/*', ['Plugin' => 'Cms', 'controller' => 'Categories', 'action' => 'display']);
+
+        $routes->connect('/articles/*', ['Plugin' => 'Cms', 'controller' => 'Articles', 'action' => 'display']);
+        $routes->connect('/categories/*', ['Plugin' => 'Cms', 'controller' => 'Categories', 'action' => 'display']);
+
         $routes->extensions(['json']);
         $routes->fallbacks('DashedRoute');
     }
