aec2f003df1e80fc358b559dce6eadbef01db2b6
George Constantinou <g.constantinou@qobo.biz>
2017-03-13 14:42:49 +0200

    update actions to require site id parameter (task #3387)

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -35,21 +35,23 @@ class CategoriesController extends AppController
                 $category->node = $tree[$category->id];
             }
         }
-        $this->set(compact('categories'));
+        $this->set(compact('categories', 'sites'));
         $this->set('_serialize', ['categories']);
     }
 
     /**
      * View method
      *
+     * @param string $siteId Site id or slug.
      * @param string|null $id Category id.
      * @return void
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function view($id = null)
+    public function view($siteId, $id = null)
     {
-        $category = $this->Categories->get($id, [
-            'contain' => ['ParentCategories', 'Articles', 'ChildCategories', 'Sites']
+        $site = $this->Categories->getSite($siteId);
+        $category = $this->Categories->getCategoryBySite($id, $site, [
+            'ParentCategories', 'Articles', 'ChildCategories', 'Sites'
         ]);
 
         $this->set('category', $category);
@@ -59,11 +61,15 @@ class CategoriesController extends AppController
     /**
      * Add method
      *
+     * @param string $siteId Site id or slug.
      * @return \Cake\Network\Response|void Redirects on successful add, renders view otherwise.
+     * @throws \InvalidArgumentException
      */
-    public function add()
+    public function add($siteId)
     {
+        $site = $this->Categories->getSite($siteId);
         $category = $this->Categories->newEntity();
+
         if ($this->request->is('post')) {
             $category = $this->Categories->patchEntity($category, $this->request->data);
             if ($this->Categories->save($category)) {
@@ -74,24 +80,28 @@ class CategoriesController extends AppController
                 $this->Flash->error(__('The category could not be saved. Please, try again.'));
             }
         }
-        $categories = $this->Categories->find('treeList', ['spacer' => self::TREE_SPACER]);
-        $sites = $this->Categories->Sites->find('list')->where(['active' => true]);
-        $this->set(compact('category', 'categories', 'sites'));
+        $categories = $this->Categories->find('treeList', [
+            'conditions' => ['Categories.site_id' => $site->id],
+            'spacer' => self::TREE_SPACER
+        ]);
+        // $sites = $this->Categories->Sites->find('list')->where(['active' => true]);
+        $this->set(compact('category', 'categories', 'site'));
         $this->set('_serialize', ['category']);
     }
 
     /**
      * Edit method
      *
+     * @param string $siteId Site id or slug.
      * @param string|null $id Category id.
      * @return \Cake\Network\Response|void Redirects on successful edit, renders view otherwise.
      * @throws \Cake\Network\Exception\NotFoundException When record not found.
      */
-    public function edit($id = null)
+    public function edit($siteId, $id = null)
     {
-        $category = $this->Categories->get($id, [
-            'contain' => ['Articles']
-        ]);
+        $site = $this->Categories->getSite($siteId);
+        $category = $this->Categories->getCategoryBySite($id, $site);
+
         if ($this->request->is(['patch', 'post', 'put'])) {
             $category = $this->Categories->patchEntity($category, $this->request->data);
             if ($this->Categories->save($category)) {
@@ -102,23 +112,29 @@ class CategoriesController extends AppController
                 $this->Flash->error(__('The category could not be saved. Please, try again.'));
             }
         }
-        $categories = $this->Categories->find('treeList', ['spacer' => self::TREE_SPACER]);
-        $sites = $this->Categories->Sites->find('list')->where(['active' => true]);
-        $this->set(compact('category', 'categories', 'sites'));
+        $categories = $this->Categories->find('treeList', [
+            'conditions' => ['Categories.site_id' => $site->id, 'Categories.id !=' => $category->id],
+            'spacer' => self::TREE_SPACER
+        ]);
+        $this->set(compact('category', 'categories', 'site'));
         $this->set('_serialize', ['category']);
     }
 
     /**
      * Delete method
      *
+     * @param string $siteId Site id or slug.
      * @param string|null $id Category id.
      * @return \Cake\Network\Response|null Redirects to index.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function delete($id = null)
+    public function delete($siteId, $id = null)
     {
         $this->request->allowMethod(['post', 'delete']);
-        $category = $this->Categories->get($id);
+
+        $site = $this->Categories->getSite($siteId);
+        $category = $this->Categories->getCategoryBySite($id, $site);
+
         if ($this->Categories->delete($category)) {
             $this->Flash->success(__('The category has been deleted.'));
         } else {
@@ -131,12 +147,13 @@ class CategoriesController extends AppController
     /**
      * Move the node.
      *
+     * @param string $siteId Site id or slug
      * @param  string $id category id
      * @param  string $action move action
      * @throws InvalidPrimaryKeyException When provided id is invalid.
      * @return \Cake\Network\Response|null
      */
-    public function moveNode($id = null, $action = '')
+    public function moveNode($siteId, $id = null, $action = '')
     {
         $moveActions = ['up', 'down'];
         if (!in_array($action, $moveActions)) {
@@ -144,12 +161,14 @@ class CategoriesController extends AppController
 
             return $this->redirect(['action' => 'index']);
         }
-        $node = $this->Categories->get($id);
+
+        $site = $this->Categories->getSite($siteId);
+        $category = $this->Categories->getCategoryBySite($id, $site);
         $moveFunction = 'move' . $action;
-        if ($this->Categories->{$moveFunction}($node)) {
-            $this->Flash->success(__('{0} has been moved {1} successfully.', $node->name, $action));
+        if ($this->Categories->{$moveFunction}($category)) {
+            $this->Flash->success(__('{0} has been moved {1} successfully.', $category->name, $action));
         } else {
-            $this->Flash->error(__('Fail to move {0} {1}.', $node->name, $action));
+            $this->Flash->error(__('Fail to move {0} {1}.', $category->name, $action));
         }
 
         return $this->redirect(['action' => 'index']);
