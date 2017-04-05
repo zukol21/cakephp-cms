2c5986a561ea0a04d6ef23ffc6f6ac179d5674d0
George Constantinou <g.constantinou@qobo.biz>
2017-04-05 15:00:26 +0300

    Added configuration file for tinyMCE plugin (task #3424)

---
+++ config/tinymce.php
@@ -0,0 +1,10 @@
+<?php
+// TinyMCE plugin configuration
+return [
+    'TinyMCE' => [
+        'plugins' => ['image'],
+        'selector' => 'textarea.tinymce',
+        'theme' => 'modern',
+        'height' => 300
+    ]
+];
