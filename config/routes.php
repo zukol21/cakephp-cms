8632054c8b0e81d386c638a8e6cfc6ceab9f045f
George Constantinou <g.constantinou@qobo.biz>
2017-04-06 16:36:53 +0300

    Articles routes should contain article type (task #3424)

--- config/routes.php
+++ config/routes.php
@@ -8,7 +8,16 @@ Router::plugin(
 
         $routes->scope('/site', function ($routes) {
             $routes->connect('/:slug/categories/:action/*', ['controller' => 'Categories'], ['pass' => ['slug']]);
-            $routes->connect('/:slug/articles/:action/*', ['controller' => 'Articles'], ['pass' => ['slug']]);
+            $routes->connect(
+                '/:slug/articles/:action/:type/*',
+                ['controller' => 'Articles'],
+                ['pass' => ['slug', 'type']]
+            );
+            $routes->connect(
+                '/:slug/category/:category/display/*',
+                ['controller' => 'Categories', 'action' => 'display'],
+                ['pass' => ['slug', 'category']]
+            );
         });
 
         $routes->fallbacks('DashedRoute');
