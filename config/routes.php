14c0873ffee0505613ee4af36286ba89129176b9
George Constantinou <g.constantinou@qobo.biz>
2017-04-10 14:50:46 +0300

    Updated articles and categories routes (task #3424)

--- config/routes.php
+++ config/routes.php
@@ -7,14 +7,25 @@ Router::plugin(
         $routes->extensions(['json']);
 
         $routes->scope('/site', function ($routes) {
-            $routes->connect('/:slug/categories/:action/*', ['controller' => 'Categories'], ['pass' => ['slug']]);
+            // Categories routes
+            $routes->connect(
+                '/:slug/categories/:action/*',
+                ['controller' => 'Categories'],
+                ['pass' => ['slug']]
+            );
+            $routes->connect(
+                '/:slug/category/:type/view/*',
+                ['controller' => 'Categories', 'action' => 'view'],
+                ['pass' => ['slug', 'type']]
+            );
+            // Articles routes
             $routes->connect(
                 '/:slug/articles/:action/:type/*',
                 ['controller' => 'Articles'],
                 ['pass' => ['slug', 'type']]
             );
             $routes->connect(
-                '/:slug/types/view/:type/*',
+                '/:slug/type/:type/view/*',
                 ['controller' => 'Articles', 'action' => 'type'],
                 ['pass' => ['slug', 'type']]
             );
