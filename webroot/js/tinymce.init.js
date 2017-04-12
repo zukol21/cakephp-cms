f1e0a99491c01da799bd9a0fc94e5cc3096a648d
George Constantinou <g.constantinou@qobo.biz>
2017-04-12 14:31:44 +0300

    fix cs error (task #3424)

--- webroot/js/tinymce.init.js
+++ webroot/js/tinymce.init.js
@@ -14,7 +14,7 @@
 
     // fix issue with link/image pop-up fields not working when tinymce is loaded within bootstrap modal
     // @link https://github.com/tinymce/tinymce/issues/782#issuecomment-151998981
-    $(document).on('focusin', function(e) {
+    $(document).on('focusin', function (e) {
         if ($(event.target).closest('.mce-window').length) {
             e.stopImmediatePropagation();
         }
