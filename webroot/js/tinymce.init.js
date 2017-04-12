06f7a7b3b905007c0fdca077d92ff427606cf359
George Constantinou <g.constantinou@qobo.biz>
2017-04-12 14:29:10 +0300

    Added fix for tinymce image and link fields not working on bs modal (task #3424)

--- webroot/js/tinymce.init.js
+++ webroot/js/tinymce.init.js
@@ -11,4 +11,12 @@
         config.file_browser_callback = elFinderBrowser;
         tinyMCE.init(config);
     });
+
+    // fix issue with link/image pop-up fields not working when tinymce is loaded within bootstrap modal
+    // @link https://github.com/tinymce/tinymce/issues/782#issuecomment-151998981
+    $(document).on('focusin', function(e) {
+        if ($(event.target).closest('.mce-window').length) {
+            e.stopImmediatePropagation();
+        }
+    });
 })(jQuery);
